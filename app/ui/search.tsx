d1e985bc1d70bb032591142b167d1b70fca043e1
johnson <johnsonsynero@gmail.com>
2025-01-16 18:21:38 +0100

    Improving accessibility

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,14 +12,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
-    params.set("page", "1");
+    params.set("page", "1"); //when we change a query it should return to 1
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     router.replace(`${pathname}?${params.toString()}`);
-  }, 300);
+  }, 300); //debouncing
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
